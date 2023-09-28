9da79d14488b096822ff194cc001be27b5a24db6
Adjeng Rara Rengganis <146207524+ararraa@users.noreply.github.com>
2023-09-28 22:31:45 +0700

    Add files via upload

---
+++ PBO2_41822010079_Latihan18.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo2_41822010079_latihan18;
+
+/**
+ *
+ * @author ASUS
+ * Nama : Adjeng Rara Rengganis 
+ * NIM  : 41822010079
+ * Nama : Vanisha Putri Kania
+ * NIM  : 41822010082
+ */
+public class PBO2_41822010079_Latihan18
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Format Tugas PBO");
+       
+        for (double i = 3.5; i <= 35.0; i += 3.5) {
+            if(i % 3.5 == 0 || 0x0 == i % 0x5)
+            System.out.println(i);
+        }
+
+        System.out.println(); 
+    }
+}
